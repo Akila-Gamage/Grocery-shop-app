404f47dbb4a2edbe78c1856e3d0597a800328e3b
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2024-12-07 22:11:01 +0530

    initial commit

---
+++ Mock23_re/Mock23_re/Mock23_reApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mock23_reApp.swift
+//  Mock23_re
+//
+//  Created by Akila Gamage on 2024-12-05.
+//
+
+import SwiftUI
+
+@main
+struct Mock23_reApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
