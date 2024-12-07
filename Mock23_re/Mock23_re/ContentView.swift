467aeacfa7552ba0efabc94fc119cf9cf29039a5
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2024-12-07 22:10:53 +0530

    initial commit

---
+++ Mock23_re/Mock23_re/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Mock23_re
+//
+//  Created by Akila Gamage on 2024-12-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomePage()
+    }
+}
+
+#Preview {
+    ContentView()
+}
